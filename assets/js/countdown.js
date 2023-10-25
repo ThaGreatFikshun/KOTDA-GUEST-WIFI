41499519d5555b0e79269124256f40eb1701f813
ThaGreatFikshun <jaycomputertechnologies@gmail.com>
2023-10-25 12:22:33 +0300

    Design update and timer added

--- assets/js/countdown.js
+++ assets/js/countdown.js
@@ -1,26 +1,26 @@
-// Set the target date for the countdown (YYYY-MM-DD HH:MM:SS)
-const targetDate = new Date('2023-12-31 23:59:59').getTime();
-
-// Update the countdown every 1 second
-const interval = setInterval(function() {
-    const currentDate = new Date().getTime();
-    const timeLeft = targetDate - currentDate;
-
-    // Calculate days, hours, minutes, and seconds
-    const days = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
-    const hours = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    const minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
-    const seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
-
-    // Display the countdown values
-    document.getElementById('days').textContent = String(days).padStart(2, '0');
-    document.getElementById('hours').textContent = String(hours).padStart(2, '0');
-    document.getElementById('minutes').textContent = String(minutes).padStart(2, '0');
-    document.getElementById('seconds').textContent = String(seconds).padStart(2, '0');
-
-    // Check if the countdown has reached zero
-    if (timeLeft < 0) {
-        clearInterval(interval); // Stop the countdown
-        document.getElementById('countdown').innerHTML = '<h2>Countdown Expired!</h2>';
+$(document).ready(function() {
+    // Set the countdown time to 2 minutes (120 seconds)
+    var countdownTime = 120;
+ 
+    // Update the countdown timer every second
+    var countdownInterval = setInterval(updateCountdown, 1000);
+ 
+    function updateCountdown() {
+        // Calculate the minutes and seconds remaining
+        var minutes = Math.floor(countdownTime / 60);
+        var seconds = countdownTime % 60;
+ 
+        // Display the minutes and seconds on the countdown timer
+        $("#minutes").text(minutes.toString().padStart(2, "0"));
+        $("#seconds").text(seconds.toString().padStart(2, "0"));
+ 
+        // Decrease the countdown time by 1 second
+        countdownTime--;
+ 
+        // If the countdown reaches 0, stop the timer and display a message
+        if (countdownTime < 0) {
+            clearInterval(countdownInterval);
+            $("#timer").html("<button>Connect</button>");
+        }
     }
-}, 1000);
+});
